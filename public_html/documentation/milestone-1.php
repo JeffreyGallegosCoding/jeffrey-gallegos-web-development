231ca9c0db49d616de58b26dda341bc554c62e4f
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-04-13 20:15:27 -0700

    Included my purpose, audience, and goal in my PWD.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,23 @@
 		<title>Milestone 1</title>
 	</head>
 	<body>
-		<ol><strong>Purpose</strong></ol>
+		<h1>Personal Website Purpose</h1>
+		<h2>Purpose, Audience, and Goal</h2>
+		<div>
+			<p>
+				<strong>Purpose:</strong> This personal website project will show off my professional experiences and
+				skills as a web developer to potential employers.
+			</p>
+			<p>
+				<strong>Audience:</strong> I have designed my website with professional web developers to be the first in mind.
+				Either free lance start up companies or industries already established in New Mexico.
+			</p>
+		<p>
+			<strong>Goal:</strong> The main goal I wanted to produce with this website, is to display my skills, work, and
+			understanding of web design and development in hopes of being hired for a full or part time position or an internship opportunity.
+		</p>
+		</div>
+		<br>
+		<h3>Persona</h3>
 	</body>
 </html>
