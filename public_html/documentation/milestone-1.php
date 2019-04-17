0bea02426a9a532da201276f4bf293ce81e8f675
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-04-16 17:00:43 -0700

    Added and started working on persona for M1.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,7 +5,7 @@
 		<title>Milestone 1</title>
 	</head>
 	<body>
-		<h1>Personal Website Purpose</h1>
+		<h1>Personal Website Milestone 1</h1>
 		<h2>Purpose, Audience, and Goal</h2>
 		<div>
 			<p>
@@ -14,14 +14,19 @@
 			</p>
 			<p>
 				<strong>Audience:</strong> I have designed my website with professional web developers to be the first in mind.
-				Either free lance start up companies or industries already established in New Mexico.
+				I am wanting to appeal to either free lance start up companies or industries already established in New Mexico.
 			</p>
 		<p>
 			<strong>Goal:</strong> The main goal I wanted to produce with this website, is to display my skills, work, and
 			understanding of web design and development in hopes of being hired for a full or part time position or an internship opportunity.
 		</p>
 		</div>
-		<br>
-		<h3>Persona</h3>
+		<h2>Persona</h2>
+		<div>
+			<p><strong>Name:</strong> Nancy L. Thomas</p>
+			<p><strong>Age:</strong> 35</p>
+			<p><strong>Profession:</strong> CTO of RS21, an established and well known technological company looking for
+			a new Fullstack Web Development Intern wanting to develop their skills and discover new and interesting fields</p>
+		</div>
 	</body>
 </html>
