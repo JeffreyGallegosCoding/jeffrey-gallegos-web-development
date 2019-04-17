d0496e4927d3d8137b3046da0a1af00e668ee7cd
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-04-16 20:43:08 -0700

    Added technology, attitudes, frustrations, goals and user story to my persona. May have to go back and make changes later.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -18,15 +18,55 @@
 			</p>
 		<p>
 			<strong>Goal:</strong> The main goal I wanted to produce with this website, is to display my skills, work, and
-			understanding of web design and development in hopes of being hired for a full or part time position or an internship opportunity.
+			understanding of web design and development in hopes of being hired for a full time, part time, or an internship opportunity.
 		</p>
 		</div>
 		<h2>Persona</h2>
 		<div>
 			<p><strong>Name:</strong> Nancy L. Thomas</p>
 			<p><strong>Age:</strong> 35</p>
-			<p><strong>Profession:</strong> CTO of RS21, an established and well known technological company looking for
-			a new Fullstack Web Development Intern wanting to develop their skills and discover new and interesting fields</p>
+			<p>
+				<strong>Profession:</strong> CTO of RS21, a continually expanding and well known technological company here in New Mexico
+				looking for a new part time to full time Front end Web Developer. They use various cutting edge technologies
+				and data design to create a platform that is easy to use and able to assist businesses solve a vast range of issues ranging
+				from crime, weather, and even political problems.
+			</p>
+			<p><strong>Technology:</strong> Power user of windows systems while working using a HP Spectre x360 laptop for
+			the office and at home. But also enjoys using her IphoneXS and Ipad Pro for work but mainly for recreational
+			uses.
+			</p>
+			<p>
+				<strong>Attitudes and Behaviors:</strong> Nancy Thomas is a very active person not just in the working community
+			but also in her social life. She works a full time position at RS21 and has a family to help take care of outside
+			of that. She still takes a little extra time at the end of her work day to look through some resumes and see if
+			there are any promising candidates for a position. Although she is more traditional and enjoys reading through well constructed
+			resumes she also believes that having a quality web presence on sites such as LinkedIn are just as important. One element
+				of online presence that she is really drawn to is a personal website which is a well organized and able to show off skills and
+				experiences in the coding profession.
+			</p>
+			<p>
+				<strong>Frustrations and Needs:</strong> Nancy is looking for some fresh talent to be part of her Front End
+			Web Development Team. She is looking for candidates to not only have a drive for functionality but also clear
+				and concise code and organized web design. With the company still growing, she is looking for and has seen a lot
+				of talented web developers but must make a decision on the next few candidates within the next week.
+				She requires that all candidates have experience in:
+			</p>
+			<ul>
+				<li>HTML, CSS, and Javascipt</li>
+				<li>Version Control data systems (Git is preferable)</li>
+				<li>Experience building single web page applications</li>
+				<li>Ability to work in a team environment and have great interpersonal skills</li>
+			</ul>
+			<p>
+				<strong>Goals:</strong> Nancy has to figure which candidates would be able to make it the next round of the
+			hiring process. Many of them have well constructed resumes, an online presence, and even personal websites. She
+			insists on using the personal websites to make the final decision. She must check carefully to see which sites
+			show the most ingenuity, creativity, and organization, up to her standards of junior developer.
+			</p>
+			<p>
+				<strong>User Story:</strong> As a power user, Nancy has to determine whether a candidate's personal website is up to
+			her standards of what a junior web developers skills should be going forward into a position at RS21.
+			</p>
 		</div>
 	</body>
 </html>
