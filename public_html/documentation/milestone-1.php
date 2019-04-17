ac0668a9e36e20c0a7bb795fb048af115e8312f5
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-04-17 15:45:27 -0700

    Edited persona for grammar and altered wording for the profession and frustration and needs sections.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,56 +16,60 @@
 				<strong>Audience:</strong> I have designed my website with professional web developers to be the first in mind.
 				I am wanting to appeal to either free lance start up companies or industries already established in New Mexico.
 			</p>
-		<p>
+			<p>
 			<strong>Goal:</strong> The main goal I wanted to produce with this website, is to display my skills, work, and
-			understanding of web design and development in hopes of being hired for a full time, part time, or an internship opportunity.
-		</p>
+			understanding of web design and development in hopes of being hired for a full time, part time, or internship opportunity.
+			</p>
 		</div>
 		<h2>Persona</h2>
 		<div>
-			<p><strong>Name:</strong> Nancy L. Thomas</p>
+			<p><strong>Name:</strong> Nancy T. Thomas</p>
 			<p><strong>Age:</strong> 35</p>
 			<p>
-				<strong>Profession:</strong> CTO of RS21, a continually expanding and well known technological company here in New Mexico
-				looking for a new part time to full time Front end Web Developer. They use various cutting edge technologies
-				and data design to create a platform that is easy to use and able to assist businesses solve a vast range of issues ranging
-				from crime, weather, and even political problems.
+				<strong>Profession:</strong> Lead Web Developer of a locally born, New Mexican start up tech company.
+				Although it began just a year ago, it has been gaining some steady footing in the community and is now in a
+				position to hire more part time and full time full stack web developers. The company specializes in all aspects
+				of web design. With an ever expanding set of departments from the front end, back end, desktop and mobile, and graphics,
+				the company would really benefit from hiring some more full stack web developers to be able assist and incorporate
+				their skills into each.
 			</p>
 			<p><strong>Technology:</strong> Power user of windows systems while working using a HP Spectre x360 laptop for
-			the office and at home. But also enjoys using her IphoneXS and Ipad Pro for work but mainly for recreational
-			uses.
+			the office and at home. But also enjoys using her IphoneXS and IPad Pro for work but mainly for recreational
+			purposes.
 			</p>
 			<p>
-				<strong>Attitudes and Behaviors:</strong> Nancy Thomas is a very active person not just in the working community
-			but also in her social life. She works a full time position at RS21 and has a family to help take care of outside
-			of that. She still takes a little extra time at the end of her work day to look through some resumes and see if
-			there are any promising candidates for a position. Although she is more traditional and enjoys reading through well constructed
-			resumes she also believes that having a quality web presence on sites such as LinkedIn are just as important. One element
-				of online presence that she is really drawn to is a personal website which is a well organized and able to show off skills and
-				experiences in the coding profession.
+				<strong>Attitudes and Behaviors:</strong> Nancy is a very active person not just in the working community
+				but also in her social life. She works a full time position and has a family to help take care of outside
+				of that. Since their company is still fairly new, she likes to take little extra time at the end of her work
+				day to look through some resumes and see if there are any promising candidates for a position.
+				Although she is using more traditional methods and enjoys reading through well constructed resumes, she also believes that
+				having a quality online presence on sites such as LinkedIn are just as important. One element of an
+				online presence that she is really drawn to is a personal website. Especially one that is well organized and
+				really able to show off the skills and experiences of the individual's coding career.
 			</p>
 			<p>
-				<strong>Frustrations and Needs:</strong> Nancy is looking for some fresh talent to be part of her Front End
-			Web Development Team. She is looking for candidates to not only have a drive for functionality but also clear
-				and concise code and organized web design. With the company still growing, she is looking for and has seen a lot
-				of talented web developers but must make a decision on the next few candidates within the next week.
-				She requires that all candidates have experience in:
+				<strong>Frustrations and Needs:</strong> Nancy is looking for some fresh talent to be part of her full stack
+				web development team. She is looking for candidates to not only have a drive for functionality, but also the ability
+				to write clear and concise code and have organized web design. With the company still growing, she has gone
+				through and looked at many potential web developers but she must be able to make a decision before
+				next week. She requires that all candidates have experience in:
 			</p>
 			<ul>
-				<li>HTML, CSS, and Javascipt</li>
-				<li>Version Control data systems (Git is preferable)</li>
-				<li>Experience building single web page applications</li>
-				<li>Ability to work in a team environment and have great interpersonal skills</li>
+				<li>The different elements of front end and back end programming such as; HTML, CSS, Javascript, PHP, and MySQL</li>
+				<li>Creating secure programs and sites for both front end and back end systems</li>
+				<li>Version control systems</li>
+				<li>Data Design and the UI/UX System</li>
+				<li>Working as a team, public speaking, and effective communication skills</li>
 			</ul>
 			<p>
-				<strong>Goals:</strong> Nancy has to figure which candidates would be able to make it the next round of the
-			hiring process. Many of them have well constructed resumes, an online presence, and even personal websites. She
-			insists on using the personal websites to make the final decision. She must check carefully to see which sites
-			show the most ingenuity, creativity, and organization, up to her standards of junior developer.
+				<strong>Goals:</strong> Nancy has to figure out which candidates would be able to make it to the next round of the
+				hiring process. Many of them have a well constructed resume, an established online presence, and astounding personal websites,
+				but she insists on using the personal websites to make the final decision. She must check carefully to see which sites
+				are the most creative, organized, and comply fully with her specific standards of what makes a great web page.
 			</p>
 			<p>
 				<strong>User Story:</strong> As a power user, Nancy has to determine whether a candidate's personal website is up to
-			her standards of what a junior web developers skills should be going forward into a position at RS21.
+				her standards of what a web developer's skills should be at going into a position at their company.
 			</p>
 		</div>
 	</body>
