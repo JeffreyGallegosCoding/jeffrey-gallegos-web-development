8423624f75de7db951a45008c3f3478b54fc6963
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-04-27 17:02:53 -0700

    Added the milestone 2 content strategy to my project.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<div>
+		<p><strong>Introduction</strong></p>
+			<p>This will be a personal introduction which may include a description of myself, my name, or maybe a
+				favorite quote that I have. I want to include a picture of myself and maybe some background imagery
+				behind the text.
+			</p>
+		</div>
+		<div>
+			<p><strong>About Me</strong></p>
+			<p>This will be a brief introduction of my skills and experiences in the technological field. I might include
+			a brief story of how I got introduced into the coding field and why, along with all of my other occupational experiences.
+				I will talk more about myself and some things that I enjoy about coding and passions outside of coding.
+			Maybe include a dream job having to do with the web development or any projects that I would like to work on.
+			</p>
+		</div>
+		<div>
+			<p><strong>Portfolio</strong></p>
+			<p>This will display all of the projects that I have worked on throughout the class. I want to add some
+			flavor to this section to really make it jump out. Maybe some cool styling in the project links, or maybe just
+			a cool picture with a description that will appear when you hover over the image. I should also include my place
+			in the project, the technology used, and a more in depth description of what we were trying to accomplish with
+			the project.
+			</p>
+		</div>
+		<div>
+			<p><strong>Contact Info</strong></p>
+			<p>This section will contain links to my email and my Github and LinkedIn profiles. I may try to also include an interactive
+			system for employers to leave their contact information on the web page if they would like, so I may be able to get in contact
+			with them as well.
+			</p>
+		</div>
+	</body>
+</html>
